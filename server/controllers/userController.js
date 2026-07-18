5e9475c9eb631c19418c451b40594750c68678ca
akhila-220207 <n220207@rguktn.ac.in>
2026-07-18 14:17:43 +0530

    added user -upadted functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -32,6 +32,42 @@ const getProfile = async (req, res) => {
 
 };
 
+
+const updateProfile = async (req, res) => {
+    try{
+        const {name,skills,education,carrerGoal}=req.body;
+        const updatedUser=await User.findByIdAndUpdate(req.user.id,{
+            name,
+            skills,
+            education,
+            carrerGoal
+        },{new:true}).select("-password");
+        if(!updatedUser){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            user: updatedUser
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+};
+
+
+
+
+
+
+
+
 module.exports = {
-    getProfile
+    getProfile,updateProfile
 };
